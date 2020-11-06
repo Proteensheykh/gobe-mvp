b54ec164e2929faf7f88754cf89100d85664e244
Proteensheykh <smomoh96@gmail.com>
2020-11-06 16:19:09 +0100

    handled error for existing emails

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -6,73 +6,74 @@ import axios from 'axios';
 import './style.css';
 
 const SignUp = () => {
-    const {register, handleSubmit, errors} = useForm();
+    const { register, handleSubmit, errors } = useForm();
     const history = useHistory();
 
     const config = {
-        headers: {'Content-Type': 'application/json'}
+        headers: { 'Content-Type': 'application/json' }
     };
 
     const onSubmit = async (data) => {
         try {
-            const res = await axios.post(`${process.env.REACT_APP_API_URL}/frontend/api/users/`, data, config);
-            console.log(res);
-            
+            await axios.post(`${process.env.REACT_APP_API_URL}/frontend/api/users/`, data, config);
+
             history.push('/welcome')
         }
         catch (err) {
-
+            if (err.response.status === 400) {
+                alert("email aleady exists");
+            }
         }
     }
 
     const dropForm = () => {
 
-        document.querySelector("#join-us").className="collapse";
-        document.querySelector("#signUpForm").className="show";
+        document.querySelector("#join-us").className = "collapse";
+        document.querySelector("#signUpForm").className = "show";
     }
 
     return (
         <div className="container">
-            
-           <button type="button" className="button" id="join-us" onClick={dropForm}>Join Us</button>
+
+            <button type="button" className="button" id="join-us" onClick={dropForm}>Join Us</button>
 
             <form onSubmit={handleSubmit(onSubmit)} className="collapse" id="signUpForm">
-                <input 
-                    placeholder='First Name' 
+                <input
+                    placeholder='First Name'
                     className="input-box"
-                    type='text' 
+                    type='text'
                     name='first_name'
-                    ref={register({required: "Name field is required", pattern: /^[A-Za-z]+$/i})}
+                    ref={register({ required: "Name field is required", pattern: /^[A-Za-z]+$/i })}
                 />
                 {errors.name && <p>{errors.name.message}</p>}
-                <br/>
+                <br />
 
-                <input placeholder='Email' 
+                <input placeholder='Email'
                     className="input-box"
-                    type='email' 
+                    type='email'
                     name='email'
                     ref={register({
-                            required: "Email field is required", 
-                            //RegEx email validation patterns
-                            pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-                            })
-                        }
+                        required: "Email field is required",
+                        //RegEx email validation patterns
+                        pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                    })
+                    }
                     required
                 />
                 {errors.email && <p>{errors.email.message}</p>}
-                <br/>
+                <br />
 
                 <div>
-                <input
-                    type='checkbox' 
-                    name='confirmed'
-                    className="confirmation-box__div"
-                    ref={register}
-                />
-                <label className="confrimation-box" htmlFor='confirmed'>Susbcribe to our newsletter</label>
+                    <input
+                        type='checkbox'
+                        name='confirmed'
+                        className="confirmation-box__div"
+                        ref={register}
+                    />
+                    <label className="confrimation-box" htmlFor='confirmed'>Susbcribe to our newsletter</label>
                 </div>
-                <input placeholder='Signup' 
-                    type='submit' 
+                <input placeholder='Signup'
+                    type='submit'
                     className="button"
                 />
             </form>
